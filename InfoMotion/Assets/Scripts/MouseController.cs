c56f7af5df66a9868d90292320b49b5631e0b12f
Gavin Auerswald <s0554139@htw-berlin.de>
2019-01-03 14:58:27 +0100

    hands connected to cube

---
+++ InfoMotion/Assets/Scripts/MouseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Util;
+
+public class MouseController : MonoBehaviour {
+
+	Controller controller;
+	int counterRight, counterLeft;
+
+	// Use this for initialization
+	void Start () {
+		controller = new Controller ();
+		counterRight = 0;
+		counterLeft = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = controller.Frame ();
+		foreach (Hand hand in frame.Hands) {
+			if (hand.IsRight) {
+				counterRight++;
+				Debug.Log("Right: " + counterRight);
+			}
+			if (hand.IsLeft) {
+				counterLeft++;
+				Debug.Log("Left: " + counterLeft);
+			}
+		}
+	}
+}
