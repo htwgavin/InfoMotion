366a2df5b424451bf4a333164720fd8b4d6a3c14
Gavin Auerswald <s0554139@htw-berlin.de>
2019-01-22 13:37:07 +0100

    added switch between scenes

--- InfoMotion/Assets/Scripts/panelScript.cs
+++ InfoMotion/Assets/Scripts/panelScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class panelScript : MonoBehaviour
@@ -134,5 +135,9 @@ public class panelScript : MonoBehaviour
         MusicPanel.SetActive(!MusicPanel.activeSelf);
 
     }
+
+	public void loadGameScene() {
+		SceneManager.LoadScene (2, LoadSceneMode.Single);
+	}
 
 }
